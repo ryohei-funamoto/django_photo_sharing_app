cd81ce33357286536a25816d21dd2f697f1ee226
Ryohei Funamoto <r.funamoto242@gmail.com>
2026-06-23 07:00:16 +0900

    feat: Post モデルを定義
    feat: Post モデルを定義
    管理画面に Post モデルを登録
    マイグレーションを実行

--- photo_sharing_app/models.py
+++ photo_sharing_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Post(models.Model):
+    posted_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'post'
+
+    def __str__(self):
+        return self.title
