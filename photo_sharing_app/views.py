8d6e58984ff40f9f40df1ab6466d002934ee9c0d
Ryohei Funamoto <r.funamoto242@gmail.com>
2026-06-27 14:12:53 +0900

    feat: 投稿作成機能のビューを定義
    feat: 投稿作成機能のビューを定義

--- photo_sharing_app/views.py
+++ photo_sharing_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def index(request):
@@ -25,5 +27,25 @@ def show(request, id):
     return render(
         request,
         'photo_sharing_app/detail.html',
+        context=context,
+    )
+
+@login_required
+def create(request):
+    form = PostForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        post = form.save(commit=False)
+        post.posted_by = request.user
+        post.save()
+        return redirect('photo_sharing_app:index')
+
+    context = {
+        'form': form,
+    }
+
+    return render(
+        request,
+        'photo_sharing_app/create.html',
         context=context,
     )
