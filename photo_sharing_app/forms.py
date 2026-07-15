bd0c15a487d014e0938c3b58dd7a6eb30ba0d492
Ryohei Funamoto <r.funamoto242@gmail.com>
2026-07-16 04:18:40 +0900

    feat: image フィールドを追加
    PostForm に image フィールドを追加

--- photo_sharing_app/forms.py
+++ photo_sharing_app/forms.py
@@ -4,14 +4,18 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content']
+        fields = ['title', 'image', 'content']
         labels = {
             'title': 'タイトル',
+            'image': '画像',
             'content': '本文',
         }
         error_messages = {
             'title': {
                 'required': 'タイトルを入力してください。',
+            },
+            'image': {
+                'required': '画像をアップロードしてください。',
             },
             'content': {
                 'required': '本文を入力してください。',
